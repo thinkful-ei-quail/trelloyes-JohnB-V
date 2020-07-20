bcd42d1590b11363f4ba297d874a2d405ed5cd91
John Bowser <john.bow7720@gmail.com>
2020-07-20 15:32:20 -0600

    functionally complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
-import list from "./List"
-function App() {
+import List from "./List";
+
+
+function App(props) {
+
+  let list = props.store.lists.map(function(list) {
+    return <List key={list.id} header={list.header} cardIds={list.cardIds} allCards={props.store.allCards} />
+  })
+
   return (
     <main className='App'>
-      {/* content goes here */}
+      <header>
+        <h1>Trelloyes!</h1>
+        <div className='App-list'>{list}</div>
+      </header>
     </main>
   );
 }
--- src/List.js
+++ src/List.js
@@ -1,66 +1,17 @@
+import React from 'react';
 import Card from './Card';
 
-export default {List:function(props)
+function List(props)
     {
-        
         let cards = props.cardIds.map(function(card){
             return <Card title={props.allCards[card].title} content={props.allCards[card].content}/>
         });
-        return (<section class="List">
+
+    return (<section class="List">
         <header class="List-header">
             <h2>{props.header}</h2>
     </header><div class="List-cards">{cards}</div></section>)
     }
-}
 
 
-/*<section class="List">
-        <header class="List-header">
-          <h2>First list</h2>
-        </header>
-        <div class="List-cards">
-          <div class="Card">
-            <button type="button">delete</button>
-            <h3>First card</h3>
-            <p>lorem ipsum</p>
-          </div>
-          <div class="Card">
-            <button type="button">delete</button>
-            <h3>Second card</h3>
-            <p>lorem ipsum</p>
-          </div>
-          <div class="Card">
-            <button type="button">delete</button>
-            <h3>Fifth card</h3>
-            <p>lorem ipsum</p>
-          </div>
-          <div class="Card">
-            <button type="button">delete</button>
-            <h3>Sixth card</h3>
-            <p>lorem ipsum</p>
-          </div>
-          <div class="Card">
-            <button type="button">delete</button>
-            <h3>Seventh card</h3>
-            <p>lorem ipsum</p>
-          </div>
-          <div class="Card">
-            <button type="button">delete</button>
-            <h3>Tenth card</h3>
-            <p>lorem ipsum</p>
-          </div>
-          <div class="Card">
-            <button type="button">delete</button>
-            <h3>Twelfth card</h3>
-            <p>lorem ipsum</p>
-          </div>
-          <div class="Card">
-            <button type="button">delete</button>
-            <h3>Thirteenth card</h3>
-            <p>lorem ipsum</p>
-          </div>
-            <button type="button" class="List-add-button">
-              + Add Random Card
-            </button>
-        </div>
-      </section>*/
+export default List;
